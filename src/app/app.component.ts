bad28dadb52c830fbfada38bbd7bba7f5bb35c63
Danyil Kravchenko <DanilStaem@gmail.com>
2020-02-13 08:45:49 +0200

    22. Передача параметров

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export  interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {
+      title: 'Хочу выучить Angular компоненты',
+      text: 'Я учу компоненты Angular',
+      id: 0
+    },
+    {
+      title: 'Куплю гараж',
+      text: 'Куплю гараж. Звоните +8-800-555-3535',
+      id: 1
+    }
+  ];
 }
