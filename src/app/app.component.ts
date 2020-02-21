e5c4444f0dadefc5e1f8a41d452eeb7089c541ac
Danyil Kravchenko <DanilStaem@gmail.com>
2020-02-21 08:15:30 +0200

    Использование операторов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+import {map, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  sub: Subscription;
+
+  constructor() {
+    const intervalSteam$ = interval(1000);
+
+    this.sub = intervalSteam$
+      .pipe(
+        filter((value => value % 2 === 0)),
+        map((value) =>
+          `Mapped value: ${value}`
+        )
+      )
+      .subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  stop() {
+    this.sub.unsubscribe();
+  }
 }
