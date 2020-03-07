8f31964e09ae274ddfda5e1dee09bff0f5313efd
Danyil Kravchenko <DanilStaem@gmail.com>
2020-03-07 17:33:50 +0200

    Routring last lesson

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post, PostsService} from '../posts.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  post: Post;
+  constructor(
+    private  route: ActivatedRoute,
+    // private postsService: PostsService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.post = data.post;
+    });
+/*    this.route.params.subscribe(param => {
+      console.log('Param ', param);
+      this.post = this.postsService.getById(+param.id);
+    });*/
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostsService} from '../posts.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+
+  showIds = false;
+  constructor(
+    public postsService: PostsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.showIds = !!params.showIds;
+    });
+
+    this.route.fragment.subscribe((fragment) => {
+      console.log(fragment);
+    });
+
+  }
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showIds: true
+      },
+      fragment: 'program-fragment'
+    });
+  }
 }
